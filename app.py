1a0fd65a2573666339a8b7452f576a3af0b5843b
toraltai <flangkg@gmail.com>
2024-04-28 15:36:13 +0600

    commit

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from student_records.database import DB_URL, db
+from student_records.telegram_api.urls import tg
 
 def create_app(test_config=None):
     app = Flask(__name__)
     app.config['JSON_AS_ASCII'] = False
     app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
 
+    app.register_blueprint(tg)
     db.init_app(app)
     return app
 
@@ -18,6 +20,9 @@ def create_database_tables(app):
 app = create_app()
 create_database_tables(app)
 
+@app.route('/')
+def index():
+    return {"Hello":"world"}
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ student_records/telegram_api/urls.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, request, jsonify
+from ..database import Student, Subscription, Payment, db, Day
+from .service import home, add_student, add_subscription, add_payment
+from datetime import datetime
+
+
+tg = Blueprint('main', __name__, url_prefix='/api')
+
+
+@tg.route('/')
+def index():
+    return home()
+
+
+@tg.route("/add", methods=["POST"])
+def post():
+    data = request.get_json()
+    student_name = data.get('student_name')
+    subscription_type = data.get('subscription_type')
+    days = data.get('days')
+
+    # Создаем объект студента
+    student = add_student(student_name)
+
+    # Создаем объект подписки
+    subscription = add_subscription(student_id=student.id, ads=subscription_type)
+
+    # Создаем объекты посещений для каждой даты
+    for date_str in days:
+        date = datetime.strptime(date_str, "%d.%m.%Y")
+        day = Day(student_id=student.id, lesson_date=date, subscription_id=subscription.id)
+        db.session.add(day)
+
+    # Создаем объект платежа
+    payment = add_payment(student_id=student.id)
+
+    return jsonify({"message": "Студент успешно создан"}), 201
