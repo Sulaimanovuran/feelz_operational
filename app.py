08d9bba7b6211685506ceb5dec64ae77dafa1c53
Sulaimanovuran <sulaimanovuran@gmail.com>
2024-04-29 00:10:16 +0600

    before changes

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ def create_app(test_config=None):
     app.config['JSON_AS_ASCII'] = False
     app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
 
+    
+
     db.init_app(app)
     return app
 
@@ -18,6 +20,9 @@ def create_database_tables(app):
 app = create_app()
 create_database_tables(app)
 
+@app.route('/')
+def index():
+    return "Hello World!"
 
 if __name__ == '__main__':
     app.run(debug=True)
